dee628eeedf72ec63eb0e690d58e4eb51ed76cd7
iremnurcolak <iremnurcolak34@gmail.com>
2023-08-01 16:54:46 +0300

    Initial commit

---
+++ src/main/java/com/lobemus/shoppingAppinventoryService/ShoppingAppInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lobemus.shoppingAppinventoryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAppInventoryServiceApplication.class, args);
+	}
+
+}
