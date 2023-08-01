dbffbead0c5cb0f9ac7a1d0da05effe28bc53664
iremnurcolak <iremnurcolak34@gmail.com>
2023-08-01 17:35:18 +0300

    Implemented controller, repository, service

--- src/main/java/com/lobemus/shoppingAppinventoryService/repository/InventoryRepository.java
+++ src/main/java/com/lobemus/shoppingAppinventoryService/repository/InventoryRepository.java
@@ -3,6 +3,9 @@ package com.lobemus.shoppingAppinventoryService.repository;
 import com.lobemus.shoppingAppinventoryService.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
+    Optional<Inventory> findBySkuCode();
 }
